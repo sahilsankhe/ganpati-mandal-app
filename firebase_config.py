fa480f7c7e51b80bb43d82d47544365bfe853980
sahilsankhe <ssankhe005@gmail.com>
2026-08-23 15:56:36 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -3,8 +3,11 @@ from firebase_admin import credentials, firestore
 import streamlit as st
 
 if not firebase_admin._apps:
-    # Secrets मधून ऑटोमॅटिक योग्य फॉरमॅटमध्ये कीज लोड होतात
+    # Secrets मधून माहिती घेऊन private key मधील newlines फिक्स करणे
     key_dict = dict(st.secrets["firebase"])
+    if "private_key" in key_dict:
+        key_dict["private_key"] = key_dict["private_key"].replace("\\n", "\n")
+        
     cred = credentials.Certificate(key_dict)
     firebase_admin.initialize_app(cred)
 
